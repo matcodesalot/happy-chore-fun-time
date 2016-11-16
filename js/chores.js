72132bddfd6e992dad72024e8a6af60139b5141f
mat <reallymathew@gmail.com>
2016-11-16 11:50:37 -0600

    move each chore to its own component, X button works

--- js/chores.js
+++ js/chores.js
@@ -2,23 +2,11 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, TextInput, View, TouchableHighlight} from 'react-native';
 import {connect} from 'react-redux';
 import * as actions from './redux/actions';
+import OneChore from './one-chore';
 
 class Chores extends Component {
-	onCheckPressed() {
-		//add 1 ticket, make the check and X button inactive, turn the background of the textBox green
-		//to disable a button: disabled={true}
-		this.props.dispatch(actions.increaseTicketCount(1));
-	}
-
-	onXPressed() {
-		//delete the chore, add 0 tickets
-		//this.props.showChore
-		this.props.dispatch(actions.removeChore());
-		console.log('-=-=-=-=-=-=-=-=-=', this.props.showChore);
-	}
-
 	render() {
-		//const show;
+		const show = this.props.showChore ? (<OneChore />) : (<View />);
 		return(
 			<View>
 				<View style={styles.topContainer}>
@@ -28,21 +16,7 @@ class Chores extends Component {
 	                </Text>
 	            </View>
 
-				<View style={styles.mainContainer}>
-					<TouchableHighlight style={styles.goodButton}
-						underlayColor='#57a85b'
-						onPress={this.onCheckPressed.bind(this)}>
-						<Text style={styles.centerText}>Check</Text>
-					</TouchableHighlight>
-
-					<Text style={styles.textBox}>Clean your room</Text>
-
-					<TouchableHighlight style={styles.badButton}
-						underlayColor='#a51313'
-						onPress={this.onXPressed.bind(this)}>
-						<Text style={styles.centerText}>X</Text>
-					</TouchableHighlight>
-				</View>
+				{show}
 
 				<View style={styles.addContainer}>
 					<TouchableHighlight
@@ -81,45 +55,6 @@ const styles = StyleSheet.create({
     centerText: {
     	alignSelf: 'center',
     },
-	mainContainer: {
-		paddingLeft: 20,
-		paddingRight: 20,
-		flexDirection: 'row',
-		marginBottom: 10,
-	},
-	goodButton: {
-		height: 36,
-		//flex: 1,
-		paddingLeft: 10,
-		paddingRight: 10,
-		backgroundColor: '#42f453',
-		borderColor: '#48BBEC',
-		borderWidth: 1,
-		//borderRadius: 8,
-		//alignSelf: 'stretch',
-		justifyContent: 'center',
-	},
-	badButton: {
-		height: 36,
-		paddingLeft: 10,
-		paddingRight: 10,
-		backgroundColor: '#ef3232',
-		borderColor: '#48BBEC',
-		borderWidth: 1,
-		justifyContent: 'center',
-		//borderRadius: 8,
-	},
-	textBox: {
-		flex: 1,
-		height: 36,
-		borderColor: '#48BBEC',
-		borderWidth: 1,
-		//borderRadius: 8,
-		// justifyContent: 'center',
-		textAlign: 'center',
-		//alignSelf: 'center',
-		marginRight: 10,
-	},
 	addContainer: {
 		marginBottom: 10,
 		justifyContent: 'center',
